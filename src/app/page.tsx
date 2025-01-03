30f08507b6cdf7dbf5dd6e1f7b9726cd0d432d4c
Mohit Hingorani <Mohithingorani2003@gmail.com>
2025-01-04 00:20:15 +0530

    added welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,28 @@
 "use client";
 
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <div></div>;
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      router.push("/signup");
+    }, 2000);
+    return () => {
+      clearTimeout(timeOut);
+    };
+  });
+  const router = useRouter();
+  return (
+    <div className="flex justify-center items-center h-screen animate-pulse">
+      <Image
+        src="/logo1.svg"
+        className=""
+        alt="logo"
+        width={200}
+        height={200}
+      />
+    </div>
+  );
 }
